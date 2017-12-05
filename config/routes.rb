a0310938e74e7e5a4c7454b199f887b3a61b7a2e
limsammy1@gmail.com <limsammy1@gmail.com>
2017-12-05 12:36:05 -0700

    cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "dashboard#index"
-  get "/auth/:provider/callback" => "sessions#create"
-  get "/signout" => "sessions#destroy", :as => :signout
+  get "/auth/:provider/callback", to: "sessions#create"
+  get "/signout", to: "sessions#destroy", as: :signout
   resources :users, only: [:new, :create]
 end
