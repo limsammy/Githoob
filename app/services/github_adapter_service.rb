bb53112964a68759cb6c7725f59081479ef206bc
limsammy1@gmail.com <limsammy1@gmail.com>
2017-12-05 16:28:50 -0700

    Functionality finished

--- app/services/github_adapter_service.rb
+++ app/services/github_adapter_service.rb
@@ -24,11 +24,26 @@ class GithubAdapterService
     JSON.parse(response.body, symbolize_names: true)[:items][0..9]
   end
 
+  def following_users
+    following_users = []
+    response = @conn.get("/users/#{@user.nickname}/following")
+    JSON.parse(response.body, symbolize_names: true).each do |user|
+      following_users << user[:login]
+    end
+    following_users
+  end
+
   def get_recent_following_commits
-    
+    following_commits = {}
+    following_users.each do |user_nickname|
+      response = @conn.get("/search/commits?q=author:#{@user_nickname}&sort=author-date")
+      following_commits[user_nickname] = JSON.parse(response.body, symbolize_names: true)[:items][0..2]
+    end
+    following_commits
   end
 
   def get_repos
-    
+    response = @conn.get("/users/#{@user.nickname}/repos?sort=updated")
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
