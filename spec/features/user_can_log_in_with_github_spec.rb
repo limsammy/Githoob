11ee4c917ffdada7e53581ec3a6e401d7a375722
limsammy1@gmail.com <limsammy1@gmail.com>
2017-12-06 14:24:40 -0700

    Finish adding vcr to existing specs

--- spec/features/user_can_log_in_with_github_spec.rb
+++ spec/features/user_can_log_in_with_github_spec.rb
@@ -7,28 +7,32 @@ feature "User logs in with Github" do
   end
 
   scenario "user can view profile page" do
-    visit root_path
+    VCR.use_cassette("profile_page") do
+      visit root_path
 
-    expect(page).to have_content 'Login with Github'
+      expect(page).to have_content 'Login with Github'
 
-    click_on "Login with Github"
+      click_on "Login with Github"
 
-    expect(current_path).to eq root_path
-    expect(page).to have_content 'Sam Lim'
-    expect(page).to have_content 'limsammy'
-    expect(page).to have_link 'Logout'
-    expect(page).to_not have_content 'Login with Github'
+      expect(current_path).to eq root_path
+      expect(page).to have_content 'Sam Lim'
+      expect(page).to have_content 'limsammy'
+      expect(page).to have_link 'Logout'
+      expect(page).to_not have_content 'Login with Github'
+    end
   end
 
   scenario "user can log out" do
-    visit root_path
+    VCR.use_cassette("logout") do
+      visit root_path
 
-    click_on "Login with Github"
+      click_on "Login with Github"
 
-    expect(page).to have_content 'Logout'
+      expect(page).to have_content 'Logout'
 
-    click_on "Logout"
+      click_on "Logout"
 
-    expect(page).to_not have_content 'Logout'
+      expect(page).to_not have_content 'Logout'
+    end
   end
 end
